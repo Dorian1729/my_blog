7178be84e4a9c800a6d2954ef0e79ce43a876a7b
Chen Siyu <15301691595@163.com>
2026-06-26 12:11:59 +0800

    两个组件

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
+function BackgroundContainer({ children }) {
+  return <main className="page">{children}</main>
+}
+
+function CenteredForm() {
+  return (
+    <form className="form">
+      <h1 className="form-title">登录</h1>
+
+      <label className="field">
+        邮箱
+        <input className="input" type="email" name="email" />
+      </label>
+
+      <label className="field">
+        密码
+        <input className="input" type="password" name="password" />
+      </label>
+
+      <button className="button" type="submit">
+        提交
+      </button>
+    </form>
+  )
+}
+
 function App() {
   return (
-    <div>
-      <h1>Hello My Blog</h1>
-    </div>
+    <BackgroundContainer>
+      <CenteredForm />
+    </BackgroundContainer>
   )
 }
 
